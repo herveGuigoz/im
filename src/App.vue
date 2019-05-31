d8387f5667b7df2b45ee3829be61366ae0937009
herveGuigoz <herve.guigoz@gmail.com>
2019-05-31 19:11:17 +0200

    nav done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainNav></MainNav>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div class="home">
+      <main-col/>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MainNav from './components/MainNav.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    MainNav,
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+@import url('./assets/fonts/fonts.css');
+* {
+    vertical-align: baseline;
+    border: 0;
+    outline: 0;
+    padding: 0;
+    margin: 0;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+}
+
+a, abbr, acronym, address, applet, big, blockquote, body, button, caption, cite, code, dd, del, dfn, div, dl, dt, em, fieldset, font, form, h1, h2, h3, h4, h5, h6, html, iframe, img, ins, kbd, label, legend, li, object, ol, p, pre, q, s, samp, small, span, strike, strong, sub, sup, table, tbody, td, tfoot, th, thead, tr, tt, ul, var {
+    /*font-family: Poppins,Helvetica,Arial,sans-serif;*/
+    color: #2b2d30;
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.4rem;
+    line-height: 1.8;
+    letter-spacing: .01em;
+    font-weight: 400;
+    font-style: normal;
+    vertical-align: baseline;
+    border: 0;
+    outline: 0;
+    padding: 0;
+    margin: 0;
+}
+.home {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: row;
+    flex-direction: row;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    -webkit-box-flex: 0;
+    -ms-flex: 0 1 auto;
+    flex: 0 1 auto;
+    flex-grow: 0;
+    flex-shrink: 1;
+    flex-basis: auto;
+}
+
+.main-content {
+  font-size: 2.5vw;
 }
 </style>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="main-nav">
+    <div class="name">
+      <div class="flex-row">
+        <span class="dot" style="transform: matrix(1, 0, 0, 1, 0, 0);"></span>
+        <h1 class="firstname">Herve</h1>
+      </div>
+      <h1 class="lastname">Guigoz</h1>
+    </div>
+  <button>ABOUT</button>
+  
+  </div>
+</template>
+<script>
+export default {
+  name: 'MainNav',
+}
+</script>
+<style scoped>
+@media (min-width: 960px){
+  #main-nav {
+    padding: 23px 35px;
+  }
+}
+#main-nav {
+    position: absolute;
+    z-index: 99;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: row;
+    flex-direction: row;
+    justify-content: space-between;
+    -webkit-box-align: start;
+    -ms-flex-align: start;
+    align-items: flex-start;
+    width: 100%;
+    top: 0;
+    left: 0;
+    pointer-events: none;
+    padding: 14px 20px;
+}
+.flex-row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: center;
+}
+.name {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding-left: 4.5rem;
+}
+.dot {
+    display: inline-block;
+    background-color: #000;
+    border-radius: 100%;
+    width: 12px;
+    height: 12px;
+    -webkit-transform: scale(0);
+    -ms-transform: scale(0);
+    transform: scale(0);
+}
+.firstname {
+    font-size: 16px;
+    letter-spacing: 4px;
+    margin-left: 0.5rem;
+    line-height: 30px;
+    font-weight: 400;
+    font-style: normal;
+    color: #4a5568;
+    text-transform: uppercase;
+}
+.lastname {
+  font-size: 20px;
+  margin-top: -4px;
+  line-height: 18px;
+  letter-spacing: 0.12em;
+  text-transform: uppercase;
+}
+button {
+    font-family: Poppins,Helvetica,Arial,sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 16px;
+    color: #000;
+    letter-spacing: .23px;
+    text-align: right;
+    -webkit-transition: color .3s ease-in-out;
+    -o-transition: color .3s ease-in-out;
+    transition: color .3s ease-in-out;
+}
+</style>
+
+
